06840bb93728b285da7e1b45391d83bcb42dc61b
JustVika <just_vika_248@bk.ru>
2023-03-06 02:47:57 +0300

    ok1

--- next.config.js
+++ next.config.js
@@ -4,3 +4,17 @@ module.exports = {
     styledComponents: true,
   },
 };
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/register',
+        destination: 'http://91.241.64.78:8088/api/auth/register', // Proxy to Backend
+      },
+      {
+        source: '/api/auth/token',
+        destination: 'http://91.241.64.78:8088/api/auth/token', // Proxy to Backend
+      },
+    ];
+  },
+};
